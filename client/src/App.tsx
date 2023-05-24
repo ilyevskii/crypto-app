5566eece08678c60509efc99c19de068619ce18e
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-25 01:56:44 +0300

    Added Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+
+
+import {Header} from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+        <Header/>
+        <Routes>
+
+        </Routes>
+    </Router>
   );
 }
 
