17a9eac7b2aa30179c379a781c505f1af53b079a
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-25 02:12:35 +0300

    Finished header component: added menu and portfolio info

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.scss';
 
 import {useNavigate} from "react-router-dom";
+import {Menu} from "./Menu/Menu";
 
 
 export function Header() {
@@ -14,6 +15,10 @@ export function Header() {
                 <h1 className="logo-text semi-bold"><span>Coin</span>Cap</h1>
                 <p className="logo-author">by @ilyevskii.</p>
             </div>
+            <Menu/>
+            <p className="portfolio-info">
+                134,32 <span className="semi-bold">USD</span> <span className="color-success">+2,38 (1,80 %)</span>
+            </p>
         </header>
     );
 }
