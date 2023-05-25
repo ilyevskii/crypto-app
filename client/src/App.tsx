e6942d657e3e1e033a2e13b7519e7ce9711f2091
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-25 03:53:54 +0300

    Added MainPage and route for it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,15 @@ import './App.scss';
 import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 
 
-import {Header} from "./components/Header/Header";
+import {Header} from "components";
+import {MainPage} from "./pages/MainPage";
 
 function App() {
   return (
     <Router basename="/">
         <Header/>
         <Routes>
-
+            <Route path="/" element={<MainPage/>}/>
         </Routes>
     </Router>
   );
---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {CryptoTable} from "components";
+
+
+export function MainPage() {
+
+    return (
+        <main className="container">
+            <CryptoTable/>
+        </main>
+    );
+}
