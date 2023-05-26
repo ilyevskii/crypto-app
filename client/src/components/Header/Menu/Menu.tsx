b09542e371de0c96dcd5781703449a7677fe62bd
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-26 19:48:36 +0300

    Added getting header currencies from api

--- client/src/components/Header/Menu/Menu.tsx
+++ client/src/components/Header/Menu/Menu.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Menu.scss';
+import {useHeaderCurrencies} from "hooks";
+import {HeaderCurrency} from "services";
 
 
 export function Menu() {
 
+    const {header_currencies, is_header_currencies_loading} = useHeaderCurrencies();
 
     return (
         <div id="menu">
             <ul className="menu-list">
-                <li className={`menu-item`}>
-                    <p>Bitcoin: <span className="semi-bold">27.000$</span></p>
-                </li>
-                <li className={`menu-item`}>
-                    <p>Etherium: <span className="semi-bold">3.000$</span></p>
-                </li>
-                <li className={`menu-item`}>
-                    <p>Ripple: <span className="semi-bold">2$</span></p>
-                </li>
+                {!is_header_currencies_loading && header_currencies ?
+                    <>
+                        {header_currencies.map((currency: HeaderCurrency) => (
+                            <li className={`menu-item`}>
+                                <p>{currency.name}: <span className="semi-bold">{currency.priceUsd}$</span></p>
+                                <p className={`color-${currency.profit ? 'success' : 'failure'}`}>
+                                    {currency.changePercent24Hr}%
+                                </p>
+                            </li>
+                        ))}
+                    </>
+                    :
+                    <div>Loading...</div>
+                }
             </ul>
         </div>
     );
