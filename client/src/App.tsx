9009b2e8207f86fb6376aa4b9bde9a93a0ad93cc
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-26 15:52:28 +0300

    Added QueryProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,19 @@ import './App.scss';
 import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 
 
-import {Header} from "components";
+import {Header, CurrencyCard} from "components";
 import {MainPage} from "./pages/MainPage";
 
 function App() {
+
   return (
-    <Router basename="/">
-        <Header/>
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-        </Routes>
-    </Router>
+      <Router basename="/">
+          <Header/>
+          <Routes>
+              <Route path="/" element={<MainPage/>}/>
+              <Route path="/currency/:id" element={<CurrencyCard/>}/>
+          </Routes>
+      </Router>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {PortfolioContextProvider} from "contexts";
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const client = new QueryClient();
+
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={client}>
+        <PortfolioContextProvider>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </PortfolioContextProvider>
+    </QueryClientProvider>
 );
