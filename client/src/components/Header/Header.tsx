400962b3e11ae3bd5da9ff7cee3feefb6cc36213
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-26 13:29:41 +0300

    Added the opening of the portfolio window by click on info

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Header.scss';
 
 import {useNavigate} from "react-router-dom";
 import {Menu} from "./Menu/Menu";
+import {ModalWindow} from "../ModalWindow/ModalWindow";
+import {PortfolioWindow} from "../PortfolioWindow/PortfolioWindow";
 
 
 export function Header() {
 
+    const [portfolioOpened, setPortfolioOpened] = useState<boolean>(false);
     const navigate = useNavigate();
 
+    const handlePortfolioClick = () => {
+        setPortfolioOpened(state => !state);
+    }
+
     return (
-        <header className="site-header">
-            <div className="logo" onClick={() => navigate("/?page=1")}>
-                <h1 className="logo-text semi-bold"><span>Coin</span>Cap</h1>
-                <p className="logo-author">by @ilyevskii.</p>
-            </div>
-            <Menu/>
-            <p className="portfolio-info">
-                134,32 <span className="semi-bold">USD</span> <span className="color-success">+2,38 (1,80 %)</span>
-            </p>
-        </header>
+        <>
+            {portfolioOpened && <ModalWindow child={<PortfolioWindow/>} onClose={handlePortfolioClick}/>}
+            <header className="site-header">
+                <div className="logo" onClick={() => navigate("/?page=1")}>
+                    <h1 className="logo-text semi-bold"><span>Coin</span>Cap</h1>
+                    <p className="logo-author">by @ilyevskii.</p>
+                </div>
+                <Menu/>
+                <button className="portfolio-info-btn" onClick={handlePortfolioClick}>
+                    134,32 <span className="semi-bold">USD</span> <span className="color-success">+2,38 (1,80 %)</span>
+                </button>
+            </header>
+        </>
     );
 }
