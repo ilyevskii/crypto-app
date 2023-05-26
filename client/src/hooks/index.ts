8c369587224f2706d4971ed9aa388bd4b91e16bf
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-26 15:53:39 +0300

    New hook for interacting with search params

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,1 +1,4 @@
 export {usePortfolioFunctions} from "./utilityHooks/usePortfolioFunctions";
+export {useSearchParams} from "./utilityHooks/useSearchParams";
+
+export {useAllCurrencies} from "./apiHooks/useAllCurrencies";
