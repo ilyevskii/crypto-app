b81b74d683fcdc39e9c66922926ecde1bad5932b
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-28 02:39:55 +0300

    Added adaptive html coding for header

--- client/src/components/Header/Menu/Menu.tsx
+++ client/src/components/Header/Menu/Menu.tsx
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import './Menu.scss';
+
 import {Currency, HeaderCurrency} from "services";
 import {useAllCurrencies} from "hooks";
+import {useMediaQuery} from "react-responsive";
 
 
 export function Menu() {
 
     const [header_currencies, setHeaderCurrencies] = useState<Currency[]>([]);
     const {crypto_currencies, is_crypto_currencies_loading} = useAllCurrencies();
+    const isSmallScreen = useMediaQuery({maxWidth: "591px"});
 
     useEffect(() => {
         if (!is_crypto_currencies_loading && crypto_currencies) setHeaderCurrencies(crypto_currencies.slice(0, 3));
@@ -21,9 +24,11 @@ export function Menu() {
                         {header_currencies.map((currency: HeaderCurrency) => (
                             <li className={`menu-item`} key={currency.id}>
                                 <p>{currency.name}: <span className="semi-bold">{currency.priceUsd}$</span></p>
-                                <p className={`color-${currency.profit ? 'success' : 'failure'}`}>
-                                    {currency.changePercent24Hr}%
-                                </p>
+                                {!isSmallScreen &&
+                                    <p className={`color-${currency.profit ? 'success' : 'failure'}`}>
+                                        {currency.changePercent24Hr}%
+                                    </p>
+                                }
                             </li>
                         ))}
                     </>
