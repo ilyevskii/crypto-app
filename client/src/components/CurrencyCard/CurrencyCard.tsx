8f12dd94b85030e46ea3a8136eff6ee5b8b26be5
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-28 06:04:36 +0300

    New CurrencyPage; Replaced all api functionality calls from CurrencyCard to CurrencyPage

--- client/src/components/CurrencyCard/CurrencyCard.tsx
+++ client/src/components/CurrencyCard/CurrencyCard.tsx
@@ -1,21 +1,15 @@
 import React, {useState} from 'react';
 import './CurrencyCard.scss';
 
-import {useParams} from "react-router-dom";
-import {Loader} from "@mantine/core";
 import {useMediaQuery} from "react-responsive";
-import {Navigate} from "react-router-dom";
 
 import {ModalWindow, ChangesSchedule, AddCurrencyWindow} from "components";
-import {useCurrencyInfo} from "hooks";
+import {CurrencyInfoType} from "hooks";
 
 
-export function CurrencyCard() {
+export function CurrencyCard({currency}: {currency: CurrencyInfoType}) {
 
-    const {id} = useParams();
     const [modalOpened, setModalOpened] = useState<boolean>(false);
-    const {currency, is_currency_loading, is_currency_error} = useCurrencyInfo(id || "1");
-
     const mw442px = useMediaQuery({maxWidth: "442px"});
 
     const handleBuyClick = () => {
@@ -24,54 +18,46 @@ export function CurrencyCard() {
 
 
     return (
-        <main className="container narrow">
+        <>
             {modalOpened && <ModalWindow child={<AddCurrencyWindow currency={currency.info}/>} onClose={handleBuyClick}/>}
-            {is_currency_error && <Navigate to={"404"}/>}
 
-            {!is_currency_loading && currency?
-                <div className="currency-card">
-                    <div className="currency-card-header">
-                        <div className="currency-card-header-logo">
-                            <h2>{currency.info.name}</h2>
-                            <p>{currency.info.symbol}</p>
-                        </div>
-                        <p className="currency-card-header-date">
-                            <span>{currency.info.date}</span>
-                            <span>{currency.info.time}</span>
-                        </p>
-                        {!mw442px &&
-                        <button
-                            className="toggle-currency-control-btn"
-                            type="button"
-                            onClick={handleBuyClick}>+</button>
-                        }
+            <div className="currency-card">
+                <div className="currency-card-header">
+                    <div className="currency-card-header-logo">
+                        <h2>{currency.info.name}</h2>
+                        <p>{currency.info.symbol}</p>
                     </div>
-                    <div className="currency-card-body">
-                        <div className="currency-card-body-info semi-bold">
-                            <p><span className="color-grey">NOW</span><span>&#36;{currency.info.priceUsd}</span></p>
-                            <p><span className="color-grey">CHANGE</span>
-                                <span className={`color-${currency.info.profit ? 'success' : 'failure'}`}>
-                                {currency.info.changePercent24Hr}&#37;
-                            </span>
-                            </p>
-                            <p><span className="color-grey">SUPPLY</span><span>&#36;{currency.info.supply}</span></p>
-                            <p><span className="color-grey">HIGH 24H</span><span>&#36;{currency.info.vwap24Hr}</span></p>
-                            <p><span className="color-grey">VOLUME</span><span>&#36;{currency.info.volumeUsd24Hr}</span></p>
-                            <p><span className="color-grey">MARKET CAP</span><span>&#36;{currency.info.marketCapUsd}</span></p>
-                        </div>
-                        <ChangesSchedule changes={currency.changes} profit={currency.info.profit}/>
-                    </div>
+                    <p className="currency-card-header-date">
+                        <span>{currency.info.date}</span>
+                        <span>{currency.info.time}</span>
+                    </p>
+                    {!mw442px &&
                     <button
-                        className="styled-btn"
+                        className="toggle-currency-control-btn"
                         type="button"
-                        onClick={handleBuyClick}>Buy Currency</button>
+                        onClick={handleBuyClick}>+</button>
+                    }
                 </div>
-                :
-                <div className="loader-wrapper">
-                   <Loader size="80px" color="yellow"/>
+                <div className="currency-card-body">
+                    <div className="currency-card-body-info semi-bold">
+                        <p><span className="color-grey">NOW</span><span>&#36;{currency.info.priceUsd}</span></p>
+                        <p><span className="color-grey">CHANGE</span>
+                            <span className={`color-${currency.info.profit ? 'success' : 'failure'}`}>
+                            {currency.info.changePercent24Hr}&#37;
+                        </span>
+                        </p>
+                        <p><span className="color-grey">SUPPLY</span><span>&#36;{currency.info.supply}</span></p>
+                        <p><span className="color-grey">HIGH 24H</span><span>&#36;{currency.info.vwap24Hr}</span></p>
+                        <p><span className="color-grey">VOLUME</span><span>&#36;{currency.info.volumeUsd24Hr}</span></p>
+                        <p><span className="color-grey">MARKET CAP</span><span>&#36;{currency.info.marketCapUsd}</span></p>
+                    </div>
+                    <ChangesSchedule changes={currency.changes} profit={currency.info.profit}/>
                 </div>
-            }
-
-        </main>
+                <button
+                    className="styled-btn"
+                    type="button"
+                    onClick={handleBuyClick}>Buy Currency</button>
+            </div>
+        </>
     );
 }
