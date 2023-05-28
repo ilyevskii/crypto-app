e3ded03c5057d99c8d2074c8532db871b6b94919
ilyevskii <ilya.andreevskii2004@gmail.com>
2023-05-28 06:10:46 +0300

    Replaces all functionality connected with api from CryptoTable to MainPage

--- client/src/components/CryptoTable/CryptoTable.tsx
+++ client/src/components/CryptoTable/CryptoTable.tsx
@@ -1,19 +1,13 @@
-import React, {MouseEvent, useEffect, useState} from 'react';
+import React, {MouseEvent, useState} from 'react';
 
 import {CryptoTableItem, ModalWindow, AddCurrencyWindow} from 'components';
 import {Currency} from 'services';
-import {useAllCurrencies, useSearchParams} from 'hooks';
-import {Loader} from "@mantine/core";
 import {useMediaQuery} from "react-responsive";
-import {Navigate} from "react-router-dom";
 
 
-export function CryptoTable() {
+export function CryptoTable({page_currencies}: {page_currencies: Currency[]}) {
 
-    const {page, setPageSearchParam} = useSearchParams();
     const [currency, setCurrency] = useState<Currency | null>(null);
-    const {crypto_currencies, is_crypto_currencies_loading, is_crypto_currencies_error} = useAllCurrencies();
-    const [page_currencies, setPageCurrencies] = useState<Currency[]>([]);
 
     const mw442px = useMediaQuery({maxWidth: "442px"});
     const mw593px = useMediaQuery({maxWidth: "53px"});
@@ -25,45 +19,31 @@ export function CryptoTable() {
         event.stopPropagation();
     }
 
-    useEffect(() => {
-        if (crypto_currencies && !is_crypto_currencies_loading) {
-            if (!page || isNaN(+page)) setPageSearchParam(1);
-            setPageCurrencies(crypto_currencies.slice((+page!-1) * 15, Math.min((+page!-1) * 15 + 15, crypto_currencies.length)));
-        }
-    }, [crypto_currencies, page])
-
 
     return (
         <>
             {currency && <ModalWindow child={<AddCurrencyWindow currency={currency}/>} onClose={handleControllerClick}/>}
-            {is_crypto_currencies_error && <Navigate to={"404"}/>}
 
-            {!is_crypto_currencies_loading && crypto_currencies ?
-                <table className="crypto-table">
-                    <thead className="table-header">
-                    <tr>
-                        {!mw741px && <th>#</th>}
-                        <th>Name</th>
-                        {!mw741px && <th>Symbol</th>}
-                        <th>Price (USD)</th>
-                        {!mw593px && <th>Supply</th>}
-                        {!mw741px && <th>VWAP (24Hr)	</th>}
-                        {!mw442px && <th>Vol (24Hr)</th>}
-                        <th>Chg (24Hr)</th>
-                        <th>Buy</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {page_currencies.map((currency: Currency) => (
-                        <CryptoTableItem currency={currency} handleClick={handleControllerClick} key={currency.id}/>
-                    ))}
-                    </tbody>
-                </table>
-                :
-                <div className="loader-wrapper">
-                    <Loader size="80px" color="yellow"/>
-                </div>
-            }
+            <table className="crypto-table">
+                <thead className="table-header">
+                <tr>
+                    {!mw741px && <th>#</th>}
+                    <th>Name</th>
+                    {!mw741px && <th>Symbol</th>}
+                    <th>Price (USD)</th>
+                    {!mw593px && <th>Supply</th>}
+                    {!mw741px && <th>VWAP (24Hr)	</th>}
+                    {!mw442px && <th>Vol (24Hr)</th>}
+                    <th>Chg (24Hr)</th>
+                    <th>Buy</th>
+                </tr>
+                </thead>
+                <tbody>
+                {page_currencies.map((currency: Currency) => (
+                    <CryptoTableItem currency={currency} handleClick={handleControllerClick} key={currency.id}/>
+                ))}
+                </tbody>
+            </table>
         </>
     );
 }
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {CryptoTable, Pagination} from "components";
+import {useAllCurrencies, useSearchParams} from "hooks";
+import {Navigate} from "react-router-dom";
+import {Loader} from "@mantine/core";
+import {Currency} from "services";
 
 
 export function MainPage() {
 
+    const {page, setPageSearchParam} = useSearchParams();
+    const {crypto_currencies, is_crypto_currencies_loading, is_crypto_currencies_error} = useAllCurrencies();
+    const [page_currencies, setPageCurrencies] = useState<Currency[]>([]);
+
+    useEffect(() => {
+        if (crypto_currencies) {
+            if (!page || isNaN(+page)) setPageSearchParam(1);
+            setPageCurrencies(crypto_currencies.slice((+page!-1) * 15, Math.min((+page!-1) * 15 + 15, crypto_currencies.length)));
+        }
+    }, [crypto_currencies, page])
+
+
     return (
         <main className="container">
-            <CryptoTable/>
+            {is_crypto_currencies_error && <Navigate to={"404"}/>}
+
+            {!is_crypto_currencies_loading && crypto_currencies ?
+                <CryptoTable page_currencies={page_currencies}/>
+                :
+                <div className="loader-wrapper">
+                    <Loader size="80px" color="yellow"/>
+                </div>
+            }
             <Pagination total={50}/>
         </main>
     );
